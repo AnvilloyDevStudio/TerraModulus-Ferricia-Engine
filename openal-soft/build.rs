3016b96a259c6513e1f5b59078b4d0289cca7a87
Ben Forge <74168521+BenCheung0422@users.noreply.github.com>
2025-05-18 23:16:55 +0800

    Add more supporting libraries

--- openal-soft/build.rs
+++ openal-soft/build.rs
@@ -6,20 +6,20 @@ use std::{env, fs};
 use std::path::PathBuf;
 
 fn main() {
-	let dst = cmake::Config::new("ode-src")
+	let dst = cmake::Config::new("openal-soft-src")
 		.define("ODE_WITH_DEMOS", "OFF")
 		.build();
 
 	println!("cargo:rustc-link-search=native={}", dst.display());
-	println!("cargo:rustc-link-lib=ode");
+	println!("cargo:rustc-link-lib=OpenAL32");
 
 	let bindings = bindgen::Builder::default()
-		.headers(fs::read_dir(dst.join("include/ode").to_str().unwrap()).unwrap()
+		.headers(fs::read_dir(dst.join("include/AL").to_str().unwrap()).unwrap()
 			.map(|v| v.unwrap().path().to_string_lossy().into_owned()))
 		.clang_arg(format!("-I{}", dst.join("include").to_str().unwrap()))
 		.generate().unwrap();
 
 	let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 	bindings.write_to_file(out_path.join("bindings.rs")).unwrap();
-	println!("cargo:rerun-if-changed=ode-src");
+	println!("cargo:rerun-if-changed=openal-soft-src");
 }
